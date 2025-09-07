8fe08a93b5b076022df0fba1e04bbc982cd31a9a
Ender <ender.karan14@gmail.com>
2025-09-07 21:28:04 +0300

    hero sayfası bitti responsive düzeltildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 import './App.css'
 
@@ -7,7 +8,10 @@ function App() {
 
   return (
     <>
-       <Navbar />
+      <Navbar />
+      <main>
+        <Hero />
+      </main>
     </>
   )
 }
