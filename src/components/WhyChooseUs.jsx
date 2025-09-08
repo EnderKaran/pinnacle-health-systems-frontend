e590dd4a7cf2b13a637858888c955b28ea3bca1d
Ender Karan <ender.karan14@gmail.com>
2025-09-08 10:58:39 +0300

    historytimeline componentini yaptim

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -48,7 +48,7 @@ const WhyChooseUs = () => {
       <div className="container mx-auto px-4">
         <div className="text-center mb-12">
           <BlurIn 
-            word="Neden RandevuAL?" 
+            word="Neden Pinnacle?" 
             className="text-3xl md:text-4xl font-bold text-gray-800"
           />
           <BlurIn 
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutHero from '../components/AboutHero';
 import AboutContent from '../components/AboutContent';
+import HistoryTimeline from '../components/HistoryTimeline'; 
 
 function AboutPage() {
   return (
     <>
       <AboutHero />
       <AboutContent />
+        <HistoryTimeline />
     </>
   )
 }
